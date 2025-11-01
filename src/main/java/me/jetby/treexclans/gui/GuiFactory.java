183362ee3ec0ac4590c035ce30fb9be0ac706b35
milim <milimon4ikyt@gmail.com>
2025-11-01 15:55:17 -0400

    Initial commit

--- src/main/java/me/jetby/treexclans/gui/GuiFactory.java
+++ src/main/java/me/jetby/treexclans/gui/GuiFactory.java
@@ -2,6 +2,7 @@ package me.jetby.treexclans.gui;
 
 import lombok.experimental.UtilityClass;
 import me.jetby.treexclans.TreexClans;
+import me.jetby.treexclans.api.gui.GuiApi;
 import me.jetby.treexclans.clan.Clan;
 import me.jetby.treexclans.clan.Member;
 import me.jetby.treexclans.clan.rank.Rank;
@@ -11,84 +12,89 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
-import java.util.HashMap;
-import java.util.Map;
 
 @UtilityClass
 public class GuiFactory {
-    private final Map<String, Gui> customGuis = new HashMap<>();
 
     public Gui create(TreexClans plugin,
                       @NotNull Menu menu,
                       @NotNull Player player,
                       @NotNull Clan clan,
                       Object... customObjects) {
-        switch (GuiType.valueOf(menu.type())) {
-            case MEMBERS -> {
-                return new MembersGui(plugin, menu, player, clan);
-            }
-            case CHOOSE_COLOR -> {
-                if (customObjects!=null) {
-                    for (Object obj : customObjects) {
-                        if (obj instanceof Member target) return new ChooseColorGui(plugin, menu, player, clan, target);
-                    }
-                } else {
-                    return new ChooseColorGui(plugin, menu, player, clan, null);
-                }
-            }
-            case CHEST -> {
-                return new ChestGui(plugin, menu, player, clan);
-            }
-            case QUESTS -> {
-                return new QuestsGui(plugin, menu, player, clan);
-            }
-            case RANKS -> {
-                return new RanksGui(plugin, menu, player, clan);
-            }
-            case RANK_PERMISSIONS -> {
-                if (customObjects!=null) {
-                    for (Object obj : customObjects) {
-                        if (obj instanceof Rank rank) return new RankPermissionsGui(plugin, menu, player, clan, rank);
-                    }
-                }
-            }
-            case CHOOSE_PLAYER_COLOR -> {return new ChoosePlayerColorGui(plugin, menu, player, clan);}
-            case MENU -> {
-                return new DefaultGui(plugin, menu, player, clan);
-            }
-            case TOP_CLANS -> {
-                if (customObjects!=null) {
-                    TopType topType = null;
-                    int num = 1;
-                    for (Object obj : customObjects) {
-                        if (obj instanceof TopType t) {
-                            topType = t;
-                        }
-                        if (obj instanceof Integer i)  {
-                            num = i;
-                        }
-                    }
-                    return new TopClansGui(plugin, menu, player, clan, topType, num);
-                }
-            }
-            default -> {
-                return getCustomGuiOrDefault(plugin, menu, player, clan, menu.type());
-            }
-        }
-        return null;
-    }
 
-    private Gui getCustomGuiOrDefault(TreexClans plugin, Menu menu, Player player, Clan clan, String type) {
-        var gui = customGuis.get(type);
-        if (gui!=null) return gui;
+        String guiType = menu.type();
+
+        Gui builtInGui = createBuiltInGui(plugin, menu, player, clan, guiType, customObjects);
+        if (builtInGui != null) {
+            return builtInGui;
+        }
+
+        Gui customGui = GuiApi.createGui(guiType, plugin, menu, player, clan, customObjects);
+        if (customGui != null) {
+            return customGui;
+        }
 
         return new DefaultGui(plugin, menu, player, clan);
     }
 
-    public void registerCustomGui(String type, Gui gui) {
-        customGuis.put(type.toUpperCase(), gui);
-    }
-    public void unregisterCustomGui(String type) {
-        customGuis.remove(type.toUpperCase());
+    /**
+     * Создает встроенные типы GUI
+     */
+    private Gui createBuiltInGui(TreexClans plugin,
+                                 @NotNull Menu menu,
+                                 @NotNull Player player,
+                                 @NotNull Clan clan,
+                                 String guiType,
+                                 Object... customObjects) {
+        return switch (GuiType.valueOf(guiType)) {
+            case MEMBERS -> new MembersGui(plugin, menu, player, clan);
+
+            case CHOOSE_COLOR -> {
+                if (customObjects != null) {
+                    for (Object obj : customObjects) {
+                        if (obj instanceof Member target) {
+                            yield new ChooseColorGui(plugin, menu, player, clan, target);
+                        }
+                    }
+                }
+                yield new ChooseColorGui(plugin, menu, player, clan, null);
+            }
+
+            case CHEST -> new ChestGui(plugin, menu, player, clan);
+
+            case QUESTS -> new QuestsGui(plugin, menu, player, clan);
+
+            case RANKS -> new RanksGui(plugin, menu, player, clan);
+
+            case RANK_PERMISSIONS -> {
+                if (customObjects != null) {
+                    for (Object obj : customObjects) {
+                        if (obj instanceof Rank rank) {
+                            yield new RankPermissionsGui(plugin, menu, player, clan, rank);
+                        }
+                    }
+                }
+                yield null;
+            }
+
+            case CHOOSE_PLAYER_COLOR -> new ChoosePlayerColorGui(plugin, menu, player, clan);
+
+            case MENU -> new DefaultGui(plugin, menu, player, clan);
+
+            case TOP_CLANS -> {
+                if (customObjects != null) {
+                    TopType topType = null;
+                    int num = 1;
+                    for (Object obj : customObjects) {
+                        if (obj instanceof TopType t) topType = t;
+                        if (obj instanceof Integer i) num = i;
+                    }
+                    yield new TopClansGui(plugin, menu, player, clan, topType, num);
+                }
+                yield null;
+            }
+
+            default -> null;
+        };
     }
 }
